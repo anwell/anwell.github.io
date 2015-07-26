2ddd10165b32d9fa29a8529aa765d332b99dff14
Anwell Wang <anwellwang@gmail.com>
2015-07-25 23:20:18 -0700

    golly code selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
+/// <reference path="../../typings/d3/d3.d.ts"/>
 var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-// If not in mobile, animate
+// If not in mobile (window width is greater than main div width), animate
 if (w > 850) {
     gol();
     hover();
@@ -19,60 +20,71 @@ function gol() {
       , w   = Math.ceil(window.innerWidth / cw) * cw // window width rounded to multiple of cell width
       , h   = Math.ceil(window.innerHeight / ch) * ch // window height rounded to multiple of cell height
 
-      , m   = false // toggle mode on mousedown/mouseup
+      , m   = true // toggle mode on mousedown/mouseup
 
       , ccx = w/cw // cell count x
       , ccy = h/ch // cell count y
-      , del = 100  // ms between generations
+      , del = 200  // ms between generations
+      , delay = 5000 // time before starting to animate
       , xs  = d3.scale.linear().domain([0, ccx]).rangeRound([0, ccx * cw])
       , ys  = d3.scale.linear().domain([0, ccy]).rangeRound([0, ccy * ch])
 
       , states = []
+      , born = d3.range(1, 7).filter(function() { return Math.random() > 0.5; })
+      , staysAlive = d3.range(1, 7).filter(function() { return Math.random() > 0.5; })
       ;
 
-    d3.range(ccx * ccy).forEach(function(c) {
-      states[c] = false;
-    });
-
-    var custom = [
-        [0, 0, 0, 1],
-        [0, 0, 0, 1],
-        [1, 1, 1, 1],
-        [0, 0, 0, 0],
-        [0, 0, 0, 1],
-        [0, 0, 0, 1],
-        [1, 1, 1, 1],
-        [0, 0, 0, 0],
-        [1, 0, 0, 1],
-        [1, 1, 0, 1],
-        [1, 1, 1, 1],
-        [0, 0, 0, 0],
-        [1, 1, 1, 0],
-        [0, 0, 0, 1],
-        [0, 1, 1, 0],
-        [0, 0, 0, 1],
-        [1, 1, 1, 0],
-        [0, 0, 0, 0],
-        [1, 1, 1, 1],
-        [0, 1, 0, 0],
-        [1, 1, 1, 1],
-        [0, 0, 0, 0],
-        [1, 1, 1, 1],
-        [1, 0, 1, 0],
-        [1, 1, 1, 1]
-    ];
-    var node = d3.select('.main').node();
-    var xOffset = Math.floor(node.offsetLeft / cw) - custom[0].length - 2;
-    var yOffset = Math.floor((node.offsetTop + 40) / ch);
-    for (var x = 0; x < custom[0].length; x++) {
-        for (var y = 0; y < custom.length; y++) {
-            states[coord(x + xOffset, y + yOffset)] = custom[y][x] == 1;
-        }
-    }
 
     var vis = d3.select('#gamecanvas').append('svg:svg')
         .attr('width', w)
         .attr('height', h);
+
+
+      function resetBoard() {
+          states = [];
+        d3.range(ccx * ccy).forEach(function(c) {
+          states[c] = false;
+        });
+
+        var custom = [
+            [0, 0, 0, 1],
+            [0, 0, 0, 1],
+            [1, 1, 1, 1],
+            [0, 0, 0, 0],
+            [0, 0, 0, 1],
+            [0, 0, 0, 1],
+            [1, 1, 1, 1],
+            [0, 0, 0, 0],
+            [1, 0, 0, 1],
+            [1, 1, 0, 1],
+            [1, 1, 1, 1],
+            [0, 0, 0, 0],
+            [1, 1, 1, 0],
+            [0, 0, 0, 1],
+            [0, 1, 1, 0],
+            [0, 0, 0, 1],
+            [1, 1, 1, 0],
+            [0, 0, 0, 0],
+            [1, 1, 1, 1],
+            [0, 0, 1, 0],
+            [0, 1, 0, 0],
+            [1, 1, 1, 1],
+            [0, 0, 0, 0],
+            [1, 1, 1, 1],
+            [1, 0, 1, 0],
+            [1, 1, 1, 1]
+        ];
+        var node = d3.select('.main').node();
+        var xOffset = Math.floor(node.offsetLeft / cw) - custom[0].length - 2;
+        var yOffset = Math.floor((node.offsetTop + 40) / ch);
+        for (var x = 0; x < custom[0].length; x++) {
+            for (var y = 0; y < custom.length; y++) {
+                states[coord(x + xOffset, y + yOffset)] = custom[y][x] == 1;
+            }
+        }
+        vis.selectAll('rect').data(states).classed('life', function(d) { return d; });
+    }
+    resetBoard();
 
     vis.selectAll('rect')
         .data(states)
@@ -100,7 +112,7 @@ function gol() {
             + states[coord(l,y)] +                      states[coord(r,y)]
             + states[coord(l,b)] + states[coord(x,b)] + states[coord(r,b)];
 
-          nextState[c = coord(x,y)] = states[c] ? n == 2 || n == 3 : n == 3;
+          nextState[c = coord(x,y)] = states[c] ? staysAlive.indexOf(n) >= 0 : born.indexOf(n) >= 0;
         }
       }
       return nextState;
@@ -115,20 +127,44 @@ function gol() {
       d3.selectAll('rect')
         .data(states = createNewGeneration())
         .classed('life', function(d) { return d; });
+        console.log(states.filter(function(d) { return d;}).length);
     }
 
     var timer;
-    setTimeout(function() {
-        timer = setInterval(animate, del);
-    }, 5000)
-
-    d3.select('body').on('click', function() {
-        console.log("TESt");
-        if (timer) {
-            clearInterval(timer);
-            timer = false;
-        } else {
+    function start(dela) {
+        setTimeout(function() {
             timer = setInterval(animate, del);
-        }
+        }, delay)
+    }
+    start(delay);
+
+    // d3.select('body').on('click', function() {
+    //     if (timer) {
+    //         clearInterval(timer);
+    //         timer = false;
+    //     } else {
+    //         timer = setInterval(animate, del);
+    //     }
+    // });
+
+    // Rule setting
+    d3.select('#born').node().value = born;
+    d3.select('#stays').node().value = staysAlive;
+    d3.select('#go').on('click', function() {
+        console.log(staysAlive + " b");
+        clearInterval(timer);
+        born = getValidValue('#born');
+        staysAlive = getValidValue('#stays');
+        console.log(born, staysAlive);
+
+        resetBoard();
+        start(0);
     });
+    function getValidValue(id) {
+        return d3.select(id).node().value = d3.select(id).node().value.split('').filter(function(character) {
+            return d3.range(1,7).join('').indexOf(character) >= 0;
+        }).map(function(character) {
+            return parseInt(character);
+        });
+    }
 }
